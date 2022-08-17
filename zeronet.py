a0da2c25dbac224420d89651ca39e6d6175e590d
caryoscelus <caryoscelus@gmx.com>
2022-08-17 12:26:51 +0000

    fix report_url

--- zeronet.py
+++ zeronet.py
@@ -101,8 +101,8 @@ def displayErrorMessage(err, error_log_path):
     res = ctypes.windll.user32.MessageBoxW(0, err_title, "ZeroNet error", MB_YESNOCANCEL | MB_ICONEXCLAIMATION)
     if res == ID_YES:
         import webbrowser
-        report_url = "https://github.com/HelloZeroNet/ZeroNet/issues/new?assignees=&labels=&template=bug-report.md&title=%s"
-        webbrowser.open(report_url % urllib.parse.quote("Unhandled exception: %s" % err_message))
+        report_url = "https://github.com/zeronet-conservancy/zeronet-conservancy/issues/new"
+        webbrowser.open(report_url)
     if res in [ID_YES, ID_NO]:
         subprocess.Popen(['notepad.exe', error_log_path])
 
