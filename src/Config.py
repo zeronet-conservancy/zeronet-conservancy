b2acdc8e47cc5dc16e77f4391059be90138af2d5
caryoscelus <caryoscelus@gmx.com>
2022-11-20 14:48:47 +0000

    use archived version of .bit domain list (deprecated) by default

--- src/Config.py
+++ src/Config.py
@@ -100,7 +100,7 @@ class Config(object):
         self.version = "0.7.7+"
         self.user_agent = "conservancy"
         # DEPRECATED ; replace with git-generated commit
-        self.rev = 5036
+        self.rev = 5037
         self.user_agent_rev = 8192
         self.argv = argv
         self.action = None
