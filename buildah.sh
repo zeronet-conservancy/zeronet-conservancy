dde03fa75c8a3f47fce4cd79a30a0ee79b1e571e
caryoscelus <caryoscelus@gmx.com>
2022-11-26 11:37:12 +0000

    initial buildah script

---
+++ buildah.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+DOCKER_BASE=python:3.10.8-alpine3.16
+ctr=$(buildah from docker.io/${DOCKER_BASE})
+mnt=$(buildah mount "$ctr")
+mkdir $mnt/znc/
+cp -r src/ $mnt/znc/
+cp -r plugins/ $mnt/znc/
+cp -r zeronet.py $mnt/znc/
+cp -r container-run-with-tor.sh $mnt/znc/
+cp -r requirements.txt $mnt/znc/
+
+# reproducibility: erase date from all the copied files
+find $mnt/znc -exec touch -d @1669459000 -m {} +
+
+buildah run $ctr apk add tor gcc libffi-dev musl-dev make openssl g++
+buildah run $ctr python3 -m pip install -r /znc/requirements.txt
+
+buildah umount "$ctr"
+
+# buildah config --entrypoint '["/znc/container-run-with-tor.sh"]' --cmd '' "$ctr"
+
+buildah commit --timestamp 1669459000 "$ctr" znc-tmp
