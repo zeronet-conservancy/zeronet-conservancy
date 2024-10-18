51df882bc639fb42b8da4a99571172e6984ab21a
caryoscelus <caryoscelus@gmx.com>
2024-10-18 09:06:10 +0000

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.python312.withPackages (ps: with ps; [
+      gevent msgpack base58 merkletools rsa pysocks pyasn1 websocket-client
+      gevent-websocket rencode bitcoinlib maxminddb pyopenssl rich defusedxml
+      pyaes coincurve
+    ]))
+  ];
+}
