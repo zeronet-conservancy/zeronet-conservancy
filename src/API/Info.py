65e6816278ed0e57f00d4f341378475212d0c2ec
caryoscelus <caryoscelus@gmx.com>
2025-04-21 16:23:56 +0000

    Info API minor code betterment

--- src/API/Info.py
+++ src/API/Info.py
@@ -1,11 +1,13 @@
-from .Util import ws_api_call, requires_permission
+from .Util import ws_api_call, requires_permission, wrap_api_reply
 
 @ws_api_call
+@wrap_api_reply
 def ping(ws, to):
     """Ping websocket connection"""
-    ws.response(to, 'pong')
+    return 'pong'
 
 @ws_api_call
+@wrap_api_reply
 def serverInfo(ws, to):
-    """Get server info"""
-    ws.response(to, ws.formatServerInfo())
+    """Get basic server info"""
+    return ws.formatServerInfo()
