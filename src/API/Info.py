e9df5b6644eaff5aa50556305bb7c1cedb42bb29
caryoscelus <caryoscelus@gmx.com>
2025-04-23 15:34:15 +0000

    API: remoteConnectionList

--- src/API/Info.py
+++ src/API/Info.py
@@ -11,3 +11,24 @@ def ping(ws, to):
 def serverInfo(ws, to):
     """Get basic server info"""
     return ws.formatServerInfo()
+
+@ws_api_call
+@requires_permission('ADMIN')
+@wrap_api_reply
+def remoteConnectionList(ws, to):
+    """List remote connections (ref old Stats plugin)"""
+    import main
+    return [
+        {
+            'id': conn.id,
+            'dir': conn.type,
+            'address': f"{conn.ip}:{conn.port}",
+            'port_open': conn.handshake.get("port_opened"),
+            'ping': conn.last_ping_delay,
+            'version': f"{conn.handshake.get('version')} r{conn.handshake.get('rev', 0)}",
+            'bytes_sent': conn.bytes_sent,
+            'bytes_recieved': conn.bytes_recv,
+            'num_sites': conn.sites,
+        }
+        for conn in main.file_server.connections()
+    ]
