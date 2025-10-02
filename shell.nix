b6755d1c363179aeb780ca4978c18aa4e2aa3bf2
caryoscelus <caryoscelus@gmx.com>
2025-10-02 13:39:29 +0000

    shell.nix: add requests

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ pkgs.mkShell {
     (pkgs.python313.withPackages (ps: with ps; [
       gevent msgpack base58 merkletools rsa pysocks pyasn1 websocket-client
       gevent-websocket rencode python-bitcoinlib maxminddb pyopenssl rich defusedxml
-      pyaes coincurve pytest typeguard
+      pyaes coincurve pytest typeguard requests
     ]))
   ];
 }
