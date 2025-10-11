b884dc76415f351e3a05c242de18beb8ffc02228
caryoscelus <caryoscelus@gmx.com>
2025-10-11 22:35:25 +0000

    Initial impl of user profile collecting

--- src/Content/User.py
+++ src/Content/User.py
@@ -1,5 +1,27 @@
-"""Handling of other user info records in db
+"""Handling of (public) user info records in db
 """
 
+from json.decoder import JSONDecodeError
+from .ContentDb import getContentDb
+
 def updateUserData(site, inner_path, content):
-    pass
+    if inner_path != 'profile/content.json':
+        return
+
+    cdb = getContentDb()
+    try:
+        profile = site.storage.loadJson('profile/profile.json')
+        username = profile['username']
+    except (FileNotFoundError, JSONDecodeError, KeyError):
+        # user failed to have a valid profile.., but we still keep or
+        # even create record because there's profile/ present
+        username = '<unknown>'
+    query = '''
+        INSERT INTO user ?
+        ON CONFLICT(address) DO
+            UPDATE SET username=excluded.username
+    '''
+    cdb.execute(query, {
+        'address': site.address,
+        'username': username,
+    })
